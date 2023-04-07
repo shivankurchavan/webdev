5a22357cfb5a6c14c5eb89bbd943b3b0310837be
shivankurchavan <61339210+shivankurchavan@users.noreply.github.com>
2023-04-07 17:24:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import "./styles.css";
+import { Routes } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import { Component } from "react";
+import "./NavbarStyles.css";
+import { MenuItems } from "./MenuItems";
+
+class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  };
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">AJAGAAL</h1>
+
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+
+        <ul className="nav-menu">
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  <i className={item.icon}></i>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+          <button>Sign Up</button>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
